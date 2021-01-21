a9a11956aa228f9ea59a48ca8c75b56466804ee9
friendly-format-0074 <samuelagasi@gmail.com>
2021-01-21 20:44:48 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -35,7 +35,7 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-  matrix[row][column]
-  matrix[row][column]= "new_value"
+  matrix[row][column]= new_value
+
 
 end
